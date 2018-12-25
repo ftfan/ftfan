215d8797da5ce568624a8b7a2091353ea620238d
imconfig <982748666@qq.com>
2018-12-25 19:08:30 +0800

    init

---
+++ data/history.ts
@@ -0,0 +1,41 @@
+interface Window {
+  FCoinHistoryData: {
+    DateTime: number;
+    TotalBaselineFT: number;
+    RevenuesEquivalentBTC: number;
+    Revenues: {
+      BTC: number;
+      ETH: number;
+      BCHABC: number;
+      LTC: number;
+      EOS: number;
+      USDT: number;
+      FT: number;
+      ETC: number;
+      XRP: number;
+      IOTA: number;
+      BCHSV: number;
+    };
+  }[];
+}
+
+window.FCoinHistoryData = [
+  {
+    DateTime: 181224,
+    TotalBaselineFT: 4256152104.589833627732257975,
+    RevenuesEquivalentBTC: 0.922665235910322723,
+    Revenues: {
+      BTC: 0.142145987371641754,
+      ETH: 7.576866155959632798,
+      BCHABC: 0.398539181297534656,
+      LTC: 1.203706078358886246,
+      EOS: 29.765674077989050492,
+      USDT: 1491.862540252507370218,
+      FT: 3187.328675124259719951,
+      ETC: 6.187878717302318126,
+      XRP: 673.297801598911062042,
+      IOTA: 2.237143998426908588,
+      BCHSV: 0.016513117678111930,
+    },
+  },
+];
